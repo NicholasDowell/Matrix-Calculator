00a810bfc7d3354ee7a06d762cdece6d99084c11
Nicholas Dowell <lykevv@gmail.com>
2018-05-12 18:27:42 -0700

    Update FirstSteps.py

--- FirstSteps.py
+++ FirstSteps.py
@@ -1,35 +1,99 @@
+##The MatrixOperations class includes a group of methods that allow the matrix to be used for row reduction operations
+# prints out all values stored in the matrix to the console
+#2
 
-#This file will be nick's practice for creating a Matrix
-#First steps are to :
-#   1: Create a 2 dimensional array
-#   2: Create methods to change the numbers stored in the Matrix
-#   3: Create methods to Print the Matrix
 
-from Matrix import Matrix
-n = Matrix(3, 5)
+def print(A):
+    row_string = "BAA"
+    for row in range(A.get_width()):
+        print(row_string)
+        for column in range(A.get_height()):
+            row_string += str(A.get_value(row, column)) + " "
+            print(row_string)
+        print(row_string)
+        row_string = ""
 
-n.replace_value(1, 0, 0)
-n.replace_value(2, 0, 1)
-n.replace_value(3, 0, 2)
-n.replace_value(4, 0, 3)
-n.print()
-print("W")
-n.add_column([7, 7, 3])
-n.print()
-print("W")
 
-print(n.get_column(2))
-print(n.get_row(2))
+# replaces a row in the matrix with the passed new row
+def replace_row(A, row_number, new_row):
+    A.get_data[row_number] = [number for number in new_row]
 
-for x in range(6):
-    n.replace_column(x, [x*x*20 % 3, (x*271) % 12, (x*403) % 11])
 
-n.print()
+"""replaces a column in the matrix with the passed new column
+#Column_number represents the index of the column in the matrix that will be replaced.
+#column_index represents the current index in the new column (the value that is being placed into the matrix)
+# the passed column must have the same number of entries as the number of rows in the matrix
+"""
 
-n.replace_row(2, [0, 0, 0, 0, 0, 0])
-n.print()
 
-n.row_interchange(0,2)
-n.print()
+def replace_column(A, column_number, new_column):
+    new_column_index = 0
+    for row in A.get_data():
+        row[column_number] = new_column[new_column_index]
+        new_column_index += 1
+    return
 
-print(n.row_is_all_zero(0))
+
+# adds a new column on the rightmost side of the matrix
+def add_column(A, new_column):
+    A.set_width(A.get_width() + 1)
+    for row in range(A.get_height()):
+        A.get_row(row).append(new_column[row])
+
+    return
+
+
+# adds a new row to the bottom of the matrix
+def add_row(A, new_row):
+    old_height = A.get_height()
+    A.set_height(old_height + 1)
+    A.get_data().append(new_row)
+
+
+# changes the stored matrix into a row reduced echelon form
+def rref(A):
+    print(1)
+    # does the thing
+
+    ## Step 1: regular echelon form
+    # all numbers below pivot are zero
+    # all pivots to the right of above pivots
+    # all nonzero rows are above all rows of zeros
+    # ECHELON LOOP
+    # Find the first nonzero column.
+    # pick a number to be the pivot in that column
+    # interchange rows to put that number in the first row
+    # create all zeros below the pivot
+    # That column is done for now. "Ignore" that column and the row its in for now.
+    # If there are more columns to echelonize, repeat loop.
+
+    ##now RREF LOOP
+    # now starting with the rightmost pivot, zero the positions above all pivots
+
+    # when all columns have zeros above and below pivots, its done! the matrix is in RREF
+
+
+# looks through each row,
+# if it contains all zeroes, interchange it with the lowest nonzero row
+def put_all_zero_rows_on_bottom(A):
+    number_of_zero_rows = 0
+    row_index = 0
+    for row in A.get_data():
+        # check if its zero
+        # if its zero, switch it
+        # then check to make sure the new one isnt zero
+        if A.row_is_all_zero(row):
+            A.row_interchange(A.get_height() - number_of_zero_rows, A.get_row(row_index))
+            number_of_zero_rows += 1
+
+        if A.row_is_all_zero(row):
+            A.row_interchange(A.get_height() - number_of_zero_rows, A.get_row(row_index))
+        row_index += 1
+
+
+def row_interchange(A, row1_index, row2_index):
+    # pass in two row numbers (INTEGERS)  those two rows of te matrix will be interchanged with each other
+    temp = A.get_row(row1_index)
+    A.set_row(row1_index, row2_index)
+    A.set_row(row2_index, temp)
+
